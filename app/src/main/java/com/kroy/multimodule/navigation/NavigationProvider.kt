c47f09c7e0e719b3f3a9c7d719c85b5a17a74068
koustavroy <koustav.roy@myelinfoundry.com>
2024-02-06 23:08:17 +0530

    Nav controller and nav builder added in ui layer of movie
    Finished at 45:15

---
+++ app/src/main/java/com/kroy/multimodule/navigation/NavigationProvider.kt
@@ -0,0 +1,7 @@
+package com.kroy.multimodule.navigation
+
+import com.feature.movie.ui.navigation.MovieApi
+
+data class NavigationProvider (
+     val movieApi : MovieApi
+)
--- feature/movie/ui/src/main/java/com/feature/movie/ui/navigation/MovieApi.kt
+++ feature/movie/ui/src/main/java/com/feature/movie/ui/navigation/MovieApi.kt
@@ -3,4 +3,5 @@ package com.feature.movie.ui.navigation
 import com.core.feature_api.FeatureApi
 
 interface MovieApi:FeatureApi {
+
 }
