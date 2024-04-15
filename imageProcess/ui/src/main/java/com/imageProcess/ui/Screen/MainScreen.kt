0c0407c788468fd4434da687cbe379014a8cf05d
koustavroy <koustav.roy@myelinfoundry.com>
2024-04-15 21:41:07 +0530

    Final image loader code with caching mechanism

--- imageProcess/ui/src/main/java/com/imageProcess/ui/Screen/MainScreen.kt
+++ imageProcess/ui/src/main/java/com/imageProcess/ui/Screen/MainScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -42,22 +43,19 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
 @OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(viewModel: MainViewModel, navController: NavController) {
-    val result = viewModel.imageList.value
+    val result by viewModel.imageList
     var page by remember { mutableStateOf(1) }
     val gridState = rememberLazyGridState()
-    val allImages = remember { mutableStateListOf<imageItem>() } // stores the downloaded data
-    /**
-     * LanuchedEffect is used to launch the block in the Coroutine Context of the composoble
-     * its used for asynchronous functionality
-     * it obseves the result
-     */
+    val allImages = remember { mutableStateListOf<imageItem>() }
+
     LaunchedEffect(result) {
         if (result != null && !result.isLoading && result.error.isBlank() && result.data != null) {
-            allImages.addAll(result.data)
+            allImages.addAll(result.data!!)
         }
     }
 
@@ -70,78 +68,38 @@ fun MainScreen(viewModel: MainViewModel, navController: NavController) {
                 val item = allImages[index]
                 ImageItem(item)
                 if (index == allImages.lastIndex && !result.isLoading && result.error.isBlank()) {
-                    // Load more images when reaching the bottom
                     page++
                     viewModel.getImageList(page)
                 }
             }
         }
 
-        if (result?.isLoading == true && allImages.isEmpty()) {
-            LoadingIndicator()
-        } else if (result?.error?.isNotBlank() == true && allImages.isEmpty()) {
-            ErrorText(result.error)
-        } else if (allImages.isEmpty()) {
-            EmptyStateText("No Images to show")
+        when {
+            result?.isLoading == true && allImages.isEmpty() -> LoadingIndicator()
+            result?.error?.isNotBlank() == true && allImages.isEmpty() -> ErrorText(result.error)
+            allImages.isEmpty() -> EmptyStateText("No Images to show")
         }
     }
 }
 
-    @Composable
-    private fun LoadingIndicator() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator()
-        }
-    }
-
-    @Composable
-    private fun ErrorText(error: String) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = error)
-        }
-    }
-
-    @Composable
-    private fun EmptyStateText(message: String) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = message)
-        }
-    }
-
-
 @Composable
 private fun ImageItem(imageData: imageItem) {
-    var bitmapState by remember { mutableStateOf<ImageBitmap?>(null) }
+    val bitmapState = remember { mutableStateOf<ImageBitmap?>(null) }
 
     LaunchedEffect(imageData.imageUrl) {
-        try {
-            val bitmap = loadImage(imageData.imageUrl)
-            bitmapState = bitmap
-        } catch (e: IOException) {
-            // Handle the error, e.g., show a placeholder image
-        }
+        val bitmap = loadImage(imageData.imageUrl)
+        bitmapState.value = bitmap
     }
 
     Box(
         modifier = Modifier
-            .height(100.dp)
-            .width(100.dp)
+            .size(100.dp)
             .border(1.dp, color = Color.Gray),
         contentAlignment = Alignment.Center
     ) {
-        val bitmap = bitmapState
-        bitmap?.let {
+        bitmapState.value?.let { bitmap ->
             Image(
-                bitmap = it,
+                bitmap = bitmap,
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxSize() // Fill the entire Box
@@ -150,19 +108,50 @@ private fun ImageItem(imageData: imageItem) {
     }
 }
 
+@Composable
+private fun LoadingIndicator() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun ErrorText(error: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = error)
+    }
+}
+
+@Composable
+private fun EmptyStateText(message: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = message)
+    }
+}
 
 private suspend fun loadImage(url: String): ImageBitmap? {
     return withContext(Dispatchers.IO) {
         try {
-
-            val connection = URL(url).openConnection() as HttpURLConnection
-            connection.doInput = true
-            connection.connect()
-            val inputStream = connection.inputStream
-            val bitmap = BitmapFactory.decodeStream(inputStream)
-            inputStream.close()
-            connection.disconnect()
-            bitmap?.asImageBitmap()
+            val bitmap = imageCache[url] ?: run {
+                val connection = URL(url).openConnection() as HttpURLConnection
+                connection.doInput = true
+                connection.connect()
+                val inputStream = connection.inputStream
+                val decodedBitmap = BitmapFactory.decodeStream(inputStream)?.asImageBitmap()
+                inputStream.close()
+                connection.disconnect()
+                decodedBitmap?.also { imageCache[url] = it }
+            }
+            bitmap
         } catch (e: IOException) {
             e.printStackTrace()
             null
@@ -170,4 +159,4 @@ private suspend fun loadImage(url: String): ImageBitmap? {
     }
 }
 
-
+val imageCache = mutableMapOf<String, ImageBitmap?>()
